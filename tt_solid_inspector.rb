31a75d62eca285bffc0fa7e40001d57eb0b66bde
Thomas Thomassen <thomas@thomthom.net>
2011-06-30 10:32:17 +0200

    Changed: Inspects the current context if nothing is selected. Cleanup: Minor code cleanup.

--- tt_solid_inspector.rb
+++ tt_solid_inspector.rb
@@ -1,17 +1,21 @@
-#-----------------------------------------------------------------------------
-# Version: 1.0.0
-# Compatible: SketchUp 7 (PC)
-#             (other versions untested)
-#-----------------------------------------------------------------------------
+#-------------------------------------------------------------------------------
+#
+# Thomas Thomassen
+# thomas[at]thomthom[dot]net
+#
+#-------------------------------------------------------------------------------
 #
 # CHANGELOG
+# 1.1.0 - 30.06.2011
+#   * Inspects current context if nothing is selected.
+#
 # 1.0.0a - 29.08.2010
-#   * Added TT_Lib2 support
+#   * Added TT_Lib2 support.
 #
 # 0.1.0a - 12.08.2010
-#   * Initial Release
+#   * Initial Release.
 #
-#-----------------------------------------------------------------------------
+#-------------------------------------------------------------------------------
 #
 # TODO:
 # Compile list of errors.
@@ -19,39 +23,37 @@
 # * Describe errors. (Open face, small edge, internal edge/face, stray edge.)
 # * Attempt to fix error automatically if possible.
 #
-#-----------------------------------------------------------------------------
-#
-# Thomas Thomassen
-# thomas[at]thomthom[dot]net
-#
-#-----------------------------------------------------------------------------
+#-------------------------------------------------------------------------------
 
 require 'sketchup.rb'
 require 'TT_Lib2/core.rb'
 
 TT::Lib.compatible?('2.0.0', 'TT Solid Inspector')
 
-#-----------------------------------------------------------------------------
+#-------------------------------------------------------------------------------
 
 module TT::Plugins::SolidInspector
   
-  ### MENU & TOOLBARS ### --------------------------------------------------
+  ### MENU & TOOLBARS ### ------------------------------------------------------
   
   unless file_loaded?( File.basename(__FILE__) )
-    m = TT.menu('Tools')
-    m.add_item('Solid Inspector')  { self.inspect_solid }
+    m = TT.menu( 'Tools' )
+    m.add_item( 'Solid Inspector' )  { self.inspect_solid }
   end
   
   
-  ### MAIN SCRIPT ### ------------------------------------------------------
+  ### MAIN SCRIPT ### ----------------------------------------------------------
 
+  # @since 1.0.0
   def self.inspect_solid
     Sketchup.active_model.tools.push_tool( SolidInspector.new )
   end
   
   
+  # @since 1.0.0
   class SolidInspector
     
+    # @since 1.0.0
     def initialize
       @instance = nil
       @errors = []
@@ -60,22 +62,34 @@ module TT::Plugins::SolidInspector
       
       @status = "Click on solids to inspect. Use arrow keys to cycle between errors. Press Return to zoom to error. Press Tab/Shift+Tab to cycle though errors and zoom."
       
-      Sketchup.active_model.selection.each { |e|
-        next unless TT::Instance.is?(e)
-        analyze(e)
-        break
-      }
+      if Sketchup.active_model.selection.empty?
+        analyze( nil )
+      else
+        Sketchup.active_model.selection.each { |e|
+          next unless TT::Instance.is?(e)
+          analyze(e)
+          break
+        }
+      end
     end
     
+    # @since 1.0.0
     def analyze(instance)
       @instance = instance
       
-      Sketchup.active_model.selection.clear
-      Sketchup.active_model.selection.add( @instance )
+      if @instance
+        Sketchup.active_model.selection.clear
+        Sketchup.active_model.selection.add( @instance )
+        entities = TT::Instance.definition(@instance).entities
+        @transformation = @instance.transformation
+      else
+        entities = Sketchup.active_model.active_entities
+        @transformation = Geom::Transformation.new()
+      end
       
       # Any edge without two faces means an error in the surface of the solid.
       @current_error = 0
-      @errors = TT::Instance.definition(@instance).entities.select { |e|
+      @errors = entities.select { |e|
         e.is_a?( Sketchup::Edge ) && e.faces.length != 2
       }
       
@@ -83,14 +97,12 @@ module TT::Plugins::SolidInspector
       @groups = []
       stack = @errors.clone
       until stack.empty?
-        #puts '...'
         cluster = []
         cluster << stack.shift
         
         # Find connected errors
         edge = cluster.first
         haystack = ([edge.start.edges + edge.end.edges] - [edge]).first & stack
-        #p haystack
         until haystack.empty?
           e = haystack.shift
           
@@ -103,25 +115,26 @@ module TT::Plugins::SolidInspector
         
         @groups << cluster
       end
-      #puts 'Groups:'
-      #p @groups
-      #p @errors
     end
     
+    # @since 1.0.0
     def activate
       Sketchup.active_model.active_view.invalidate
       Sketchup.status_text = @status
     end
     
+    # @since 1.0.0
     def deactivate(view)
       view.invalidate
     end
     
+    # @since 1.0.0
     def resume(view)
       view.invalidate
       Sketchup.status_text = @status
     end
     
+    # @since 1.0.0
     def onLButtonUp(flags, x, y, view)
       ph = view.pick_helper
       ph.do_pick(x, y)
@@ -131,7 +144,7 @@ module TT::Plugins::SolidInspector
       view.invalidate
     end
 
-    
+    # @since 1.0.0
     def onKeyUp(key, repeat, flags, view)
       return if @groups.empty?
       
@@ -164,18 +177,20 @@ module TT::Plugins::SolidInspector
       view.invalidate
     end
     
+    # @since 1.0.0
     def zoom_to_error(view)
       e = @groups[ @current_error ]
       view.zoom( e )
       # Adjust camera for the instance transformation
       camera = view.camera
-      t = @instance.transformation
+      t = @transformation
       eye = camera.eye.transform( t )
       target = camera.target.transform( t )
       up = camera.up.transform( t )
       view.camera.set( eye, target, up )
     end
     
+    # @since 1.0.0
     def draw(view)
       view.line_width = 3
       view.line_stipple = ''
@@ -188,7 +203,7 @@ module TT::Plugins::SolidInspector
           
           # Get points for each error edge
           pts = error.map { |e| e.vertices.map{|v|v.position} }.flatten
-          pts.map! { |pt| pt.transform( @instance.transformation ) }
+          pts.map! { |pt| pt.transform( @transformation ) }
           
           view.draw(GL_LINES, pts)
           
@@ -209,15 +224,32 @@ module TT::Plugins::SolidInspector
   end # class SolidInspector
 
   
-  ### HELPER METHODS ### ---------------------------------------------------
+  ### HELPER METHODS ### -------------------------------------------------------
 
   
-  def self.reload
+  # @note Debug method to reload the plugin.
+  #
+  # @example
+  #   TT::Plugins::Template.reload
+  #
+  # @param [Boolean] tt_lib
+  #
+  # @return [Integer]
+  # @since 1.0.0
+  def self.reload( tt_lib = false )
+    original_verbose = $VERBOSE
+    $VERBOSE = nil
+    TT::Lib.reload if tt_lib
+    # Core file (this)
     load __FILE__
+  ensure
+    $VERBOSE = original_verbose
   end
   
 end # module
 
-#-----------------------------------------------------------------------------
+#-------------------------------------------------------------------------------
+
 file_loaded( File.basename(__FILE__) )
-#-----------------------------------------------------------------------------
+
+#-------------------------------------------------------------------------------
